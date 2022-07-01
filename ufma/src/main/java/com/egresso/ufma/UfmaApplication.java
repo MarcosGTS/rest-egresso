f0993e540fd9960cad096cf75d77eea30d97a841
MarcosGTS <marcosgtsodre@gmail.com>
2022-06-30 21:04:22 -0300

    add webConfigurator

--- ufma/src/main/java/com/egresso/ufma/UfmaApplication.java
+++ ufma/src/main/java/com/egresso/ufma/UfmaApplication.java
@@ -2,10 +2,19 @@ package com.egresso.ufma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class UfmaApplication {
+@EnableWebMvc
+public class UfmaApplication implements WebMvcConfigurer {
 
+	@Override
+	public void addCorsMappings(CorsRegistry cors) {
+		cors.addMapping("/**")
+			.allowedMethods("GET", "PUT", "DELETE", "POST", "OPTIONS");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(UfmaApplication.class, args);
 	}
